164615fbdf567710d8d2c4bd2c4eec1dc4e7b1d9
MayD524 <crossroadsact@gmail.com>
2025-03-11 19:01:47 -0400

    bm/unlocked v1.0.1

--- src/core/systems/Scripts.js
+++ src/core/systems/Scripts.js
@@ -1,6 +1,10 @@
 import { System } from './System'
 
 import * as THREE from '../extras/three'
+import {Water} from "three/addons/objects/Water.js"
+import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
+
+
 import { DEG2RAD, RAD2DEG } from '../extras/general'
 import { clamp, num, uuid } from '../utils'
 import { LerpVector3 } from '../extras/LerpVector3'
@@ -20,7 +24,6 @@ export class Scripts extends System {
     this.compartment = new Compartment({
       console: {
         log: (...args) => console.log(...args),
-        warn: (...args) => console.warn(...args),
         error: (...args) => console.error(...args),
         time: (...args) => console.time(...args),
         timeEnd: (...args) => console.timeEnd(...args),
@@ -36,20 +39,37 @@ export class Scripts extends System {
       lockdown: undefined,
       num,
       clamp,
+      structuredClone,
       // Layers,
       Object3D: THREE.Object3D,
       Quaternion: THREE.Quaternion,
       Vector3: THREE.Vector3,
       Euler: THREE.Euler,
       Matrix4: THREE.Matrix4,
+
+      // Exposing really cool things :)
+      THREE,
+      Water,
+      CSS3DRenderer,
+      CSS3DObject,
+      gThis: globalThis,
+
       LerpVector3,
       LerpQuaternion,
+      GameWorld: world,
       // Material: Material,
       // Curve: Curve,
       // Gradient: Gradient,
       DEG2RAD,
       RAD2DEG,
       uuid,
+
+      // Allow for wasm
+      WebAssembly: {
+        instantiate: WebAssembly.instantiate,
+        Memory: WebAssembly.Memory,
+        Table: WebAssembly.Table,
+      },
       // pause: () => this.world.pause(),
     })
   }
